e4a2ab4ae9eabc11856c93962b0de7adda909de1
Matheus <marquesinimatheuss@hotmail.com>
2020-06-18 15:00:31 -0300

    Adicionado a API

---
+++ API_emezu/API_emezu/Controllers/UsuarioController.cs
@@ -0,0 +1,58 @@
+﻿using API_emezu.App_Start;
+using API_emezu.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_emezu.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        SqlConnection con = conexao.obterConexao();
+        private static List<Usuario> usuarios = new List<Usuario>();
+
+        public DataTable Get()
+        {
+            string query = "SELECT * FROM usuario";
+            SqlCommand cmd = new SqlCommand(query, con);
+            conexao.obterConexao();
+            cmd.CommandType = CommandType.Text;
+            //cmd.ExecuteNonQuery();     
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable usuario = new DataTable();
+            da.Fill(usuario);          
+            return usuario;
+        }
+
+        public void Post(Usuario user)
+        {
+            string query = "INSERT INTO usuario(nome, endereco, telefone) VALUES ('"+user.nome+"','"+user.endereco+"','"+user.telefone+"');";
+            SqlCommand cmd = new SqlCommand(query, con);
+            conexao.obterConexao();
+            cmd.CommandType = CommandType.Text;
+            cmd.ExecuteNonQuery();
+            conexao.fecharConexao();
+            //usuarios.Add(user);
+        }
+
+        public IHttpActionResult Delete(int Id)
+        {
+            if (Id <= 0)
+                return BadRequest("Not a valid id");
+            string query = "DELETE FROM usuario WHERE Id=" + Id;
+            SqlCommand cmd = new SqlCommand(query, con);
+            conexao.obterConexao();
+            cmd.CommandType = CommandType.Text;
+            cmd.ExecuteNonQuery();
+            conexao.fecharConexao();
+            //Usuario usuario = usuarios.Find(delegate(Usuario us) { return us.Id== Id; });
+            //usuarios.Remove(usuario);
+            return Ok();
+        }
+    }
+}
---
+++ API_emezu/API_emezu/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_emezu.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string nome { get; set; }
+        public string telefone  { get; set; }
+        public string endereco { get; set; }
+
+        /*public Usuario(string text)
+        {
+            this.nome = text;
+        }*/
+    }
+}
